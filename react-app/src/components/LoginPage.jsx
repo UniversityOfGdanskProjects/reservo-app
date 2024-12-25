27ec4346c5339d2c7eddb88b54d67c8d5e19daf7
Amelia Kanabaj <a.kanabaj.124@studms.ug.edu.pl>
2024-12-25 11:11:31 +0100

    added styles for login page and header

--- react-app/src/components/LoginPage.jsx
+++ react-app/src/components/LoginPage.jsx
@@ -15,21 +15,17 @@ export default function LoginPage() {
   return (
     <div className="registry-login">
       <div>
-        <div>
-          <input type="email" placeholder="adres email" id="email" name="email" required />
-        </div>
-        <div>
-          <input type="password" placeholder="hasło" id="password" name="password" required />
-        </div>
-        <button className="login" onClick={handleLoginClick}>
-          Zaloguj się
-        </button>
+        <input type="email" placeholder="adres email" id="email" name="email" required />
       </div>
       <div>
-        <button className="account" onClick={handleRegistryClick}>
-          Utwórz konto
-        </button>
+        <input type="password" placeholder="hasło" id="password" name="password" required />
       </div>
+      <button className="login" onClick={handleLoginClick}>
+        Zaloguj się
+      </button>
+      <button className="account" onClick={handleRegistryClick}>
+        Utwórz konto
+      </button>
     </div>
   );
 }
