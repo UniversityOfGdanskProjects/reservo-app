fa5125680d2e11d3b1d14c541bc13c33822beb8e
Amelia Kanabaj <a.kanabaj.124@studms.ug.edu.pl>
2024-12-25 13:35:32 +0100

    created backend folder and basic api. Possible creating accounts

--- react-app/src/components/LoginPage.jsx
+++ react-app/src/components/LoginPage.jsx
@@ -1,24 +1,63 @@
 import React from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function LoginPage() {
   const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [userInfo, setUserInfo] = useState(null);
 
-  const handleLoginClick = () => {
-    navigate('/home'); 
-  };
+  // const handleLoginClick = () => {
+  //   navigate('/home'); 
+  // };
 
   const handleRegistryClick = () => {
     navigate('/registry');
   }
 
+  const handleLoginClick = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch('http://localhost:3000/api/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      setUserInfo(data); // Zapisujemy pełne dane użytkownika
+      localStorage.setItem('userInfo', JSON.stringify(data));
+      console.log('Zalogowano:', data); // Debugowanie
+      alert('Zalogowano pomyślnie!');
+    } else {
+      alert('Błąd logowania.');
+    }
+  };
+
   return (
     <div className="registry-login">
       <div>
-        <input type="email" placeholder="adres email" id="email" name="email" required />
+        <input 
+          type="email" 
+          placeholder="adres email" 
+          id="email" name="email" 
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required 
+        />
       </div>
       <div>
-        <input type="password" placeholder="hasło" id="password" name="password" required />
+        <input 
+          type="password" 
+          placeholder="hasło" 
+          id="password" 
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required 
+        />
       </div>
       <button className="login" onClick={handleLoginClick}>
         Zaloguj się
