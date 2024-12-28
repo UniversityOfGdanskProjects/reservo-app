8dd5466933788e7f94454dbe2c3cd372145fae01
Amelia Kanabaj <a.kanabaj.124@studms.ug.edu.pl>
2024-12-28 13:05:07 +0100

    added a display of reservations on the reservations page

--- react-app/src/components/Reservations.jsx
+++ react-app/src/components/Reservations.jsx
@@ -2,5 +2,26 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function Reservations() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
+    const [reservations, setReservations] = useState([]);
+
+    useEffect(() => {
+        const storedReservations = localStorage.getItem('reservations');
+        if (storedReservations) {
+            const reservations = JSON.parse(storedReservations);
+            console.log(reservations);//debug
+            setReservations(reservations);
+        }
+    }, []); 
+
+    return (
+        <div className='reservations-page'>
+             <h3>Twoje rezerwacje:</h3>
+            {reservations.map((reservation, index) => (
+                <div key={index}>
+                    Data: {reservation.date}, Godzina: {reservation.time}
+                </div>
+            ))}
+        </div>
+    );
 }
