bf5b7e8a6d62e4d71e526a4b559e611415d6abe6
Amelia Kanabaj <a.kanabaj.124@studms.ug.edu.pl>
2025-01-06 17:20:16 +0100

    added reservation status to admins reservations

--- react-app/src/components/AdminReservations.jsx
+++ react-app/src/components/AdminReservations.jsx
@@ -36,15 +36,43 @@ export default function AdminReservations() {
         initialize();
     }, []); 
 
-    useEffect(() => {
-        const storedReservations = localStorage.getItem('reservations');
-        if (storedReservations) {
-            const reservations = JSON.parse(storedReservations);
-            setReservations(reservations);
-            if (reservations.length >= 1) {
+    const fetchReservations = async () => {
+        try {
+            const response = await fetch('http://localhost:3000/api/reservations');
+            if (!response.ok) {
+                throw new Error('Nie udało się pobrać rezerwacji');
+            }
+            const data = await response.json();
+            //const filtered = data.filter((reservation) => reservation.userId == currentUserId);
+            const reservationsWithStatus = data.map((el) => {
+                const { restaurant, date, time, userId } = el;
+                console.log(el.restaurant)//debug
+                const [hour, minute] = el.time.split(':').map(Number);
+                const reservationDate = new Date(el.date); 
+                reservationDate.setHours(hour, minute, 0, 0);
+                const today = new Date();
+                console.log(today);//debug
+                console.log(reservationDate);
+                let status = 'active'
+                if (reservationDate >= today) {
+                    status = 'active';
+                  } else {
+                    status = 'expired';
+                  }
+                const reservation = {restaurant, date, time, userId, status};
+                return reservation;
+            });
+            setReservations(reservationsWithStatus);
+            if (reservationsWithStatus.length >= 1) {
                 setReservationsEmpty(false);
             }
+        } catch (error) {
+            console.log('Błąd podczas pobierania rezerwacji:', error);
         }
+      };
+
+    useEffect(() => {
+        fetchReservations();
     }, []); 
 
     const handleDeleteClick = async (reservationObject) => {
@@ -92,32 +120,36 @@ export default function AdminReservations() {
             {loading}
             {isUserAdmin === true && (
                 <div className='reservations-page'>
-                {reservationsEmpty === true && (
-                    <div className='no-reservations'>
-                        <div>Nie ma żadnych rezerwacji</div>
-                    </div>
-                )}
-                {reservationsEmpty === false && (
-                    <div className='reservations'>
-                        <h3>Rezerwacje:</h3>
-                        <ul>
-                            {reservations.map((reservation, index) => (
-                                <li key={index}>
-                                    <div>
-                                        <b> Restauracja:</b> {reservation.restaurant.name}, 
-                                        <b> Data: </b>{reservation.date}, 
-                                        <b> Godzina:</b> {reservation.time}, 
-                                        <b> ID restauracji:</b> {reservation.restaurant.id} ,
-                                        <b> ID użytkownika:</b> {reservation.userId}
-                                    </div>
-                                    <button onClick={() => handleDeleteClick(reservation)}>Usuń</button>
-                                    <button onClick={() => handleChangeClick(reservation)}>Zmień</button>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                )}
-            </div>
+                    {reservationsEmpty === true && (
+                        <div className='no-reservations'>
+                            <div>Nie ma żadnych rezerwacji</div>
+                        </div>
+                    )}
+                    {reservationsEmpty === false && (
+                        <div className='reservations'>
+                            <h3>Rezerwacje:</h3>
+                            <ul>
+                                {reservations.map((reservation, index) => (
+                                    <li key={index}>
+                                        <div>
+                                            <b> Restauracja:</b> {reservation.restaurant.name}, 
+                                            <b> Data: </b>{reservation.date}, 
+                                            <b> Godzina:</b> {reservation.time}, 
+                                            <b> ID restauracji:</b> {reservation.restaurant.id},
+                                            <b> ID użytkownika:</b> {reservation.userId},
+                                            <b> Status: </b>
+                                            <span className={reservation.status === 'active' ? 'active' : 'expired'}>
+                                                {reservation.status}
+                                            </span>
+                                        </div>
+                                        <button onClick={() => handleDeleteClick(reservation)}>Usuń</button>
+                                        <button onClick={() => handleChangeClick(reservation)}>Zmień</button>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
+                </div>
             )}
             {isUserAdmin === false && <div>Dostęp tylko dla administratorów</div>}
         </>
