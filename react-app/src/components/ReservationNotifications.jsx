379bf42493cc2a913c4c170c92140676805bfbe1
Amelia Kanabaj <a.kanabaj.124@studms.ug.edu.pl>
2025-01-23 18:07:31 +0100

    changed scss for the Search component

--- react-app/src/components/ReservationNotifications.jsx
+++ react-app/src/components/ReservationNotifications.jsx
@@ -4,20 +4,20 @@ import emailjs from 'emailjs-com';
 const ReservationNotifications = () => {
     const sendEmail = async (toEmail, reservation) => {
         const templateParams = {
-            to_email: toEmail,                           // Dynamiczny adres e-mail użytkownika
-            restaurant_name: reservation.restaurant.name, // Nazwa restauracji
-            reservation_date: reservation.date,           // Data rezerwacji
-            reservation_time: reservation.time,           // Godzina rezerwacji
-            restaurant_city: reservation.restaurant.city, // Miasto restauracji
-            restaurant_address: reservation.restaurant.adress // Adres restauracji
+            to_email: toEmail,                           
+            restaurant_name: reservation.restaurant.name, 
+            reservation_date: reservation.date,     
+            reservation_time: reservation.time,  
+            restaurant_city: reservation.restaurant.city, 
+            restaurant_address: reservation.restaurant.adress 
         };
 
         try {
             const response = await emailjs.send(
-                'service_yvqvc86',  // Twój Service ID z EmailJS
-                'template_u9x6etr', // Twój Template ID
+                'service_yvqvc86',  //serviceID
+                'template_u9x6etr', //templateID
                 templateParams,
-                '0zNaibg2kPW4j-Cuv' // Twój Public Key (User ID)
+                '0zNaibg2kPW4j-Cuv' //Public Key
             );
             
 
@@ -35,13 +35,10 @@ const ReservationNotifications = () => {
             const reservationDateTime = new Date(`${reservation.date} ${reservation.time}`);
             const timeDifference = reservationDateTime - now;
 
-            // Sprawdzamy, czy rezerwacja przypada za godzinę
-            if (timeDifference > 0 && timeDifference == 3600000) {
+            if (timeDifference > 0 && timeDifference <= 3600000 && timeDifference >= 3500000) {
                 try {
-                    // Pobieramy adres e-mail użytkownika (przy założeniu, że jest w reservation.userEmail)
                     const userEmail = await fetchUserEmail(reservation.userId);
 
-                    // Wysyłamy e-mail
                     await sendEmail(userEmail, reservation);
                 } catch (error) {
                     console.error('Błąd w procesie sprawdzania rezerwacji:', error);
@@ -74,7 +71,7 @@ const ReservationNotifications = () => {
             const users = await response.json();
             const user = users.find(user => user.id == userId);
             console.log(user.email);//debug
-            return user.email; // Zwracamy e-mail użytkownika
+            return user.email;
         } catch (error) {
             console.error('Błąd podczas pobierania e-maila użytkownika:', error);
             return null;
@@ -84,12 +81,12 @@ const ReservationNotifications = () => {
     useEffect(() => {
         const interval = setInterval(() => {
             fetchReservations();
-        }, 60000); // Sprawdzamy rezerwacje co minutę
+        }, 60000);
 
         return () => clearInterval(interval);
     }, []);
 
-    return null; // Ten komponent działa tylko w tle
+    return null; 
 };
 
 export default ReservationNotifications;
